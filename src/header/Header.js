fc631422d8a299ce9e0cc347ba03560c47384b71
Richa Chakraborty <151943279+RichaChakra@users.noreply.github.com>
2024-07-13 21:03:45 +0530

    project

---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Hello This is Richa and I am learning raect with js and this is my 1st react project with proper ostadi and paglami
+      </h1>
+    </div>
+  )
+}
+
+export default Header
